98f90bf2767b2ce33e45847bac58d2400957e452
rasooly-dev <alofiabdulprogramming@gmail.com>
2023-10-28 13:38:42 -0700

    Add Upload Image Logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,31 @@
-import ImageUpload from './components/ImageUpload'
+"use client"
+
 import ImageForm from './components/ImageForm'
 import styles from './LandingPage.module.css'
 
 const UploadPage = () => {
+
+  const handleImageUpload = async (file: File) => {
+    const sendImageUrl = new URL(`${process.env.NEXT_PUBLIC_CONVEX_URL}/sendImage`)
+
+    fetch(sendImageUrl.toString(), {
+      method: 'POST',
+      headers: { 'Content-Type': file.type},
+      body: file
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+  }
+
   return (
     <div className={styles.page}>
       <h1> Upload Image </h1>
-      <ImageForm />
+      <ImageForm handleUpload={handleImageUpload}/>
     </div>
   )
 } 
