32d9eebfcf70dd705446fa1740d52621aeebfa9b
rasooly-dev <alofiabdulprogramming@gmail.com>
2023-10-28 13:01:16 -0700

    Fix Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import ImageUpload from './components/ImageUpload'
-import './globals.css'
+import styles from './Landing.module.css'
+
 
 const UploadPage = () => {
   return (
-    <div>
-      <h1> Upload Image </h1>
-      <ImageUpload />
+    <div className={styles.page}>
+      <section>
+        <h1> Upload Image </h1>
+        <ImageUpload />
+      </section>
     </div>
   )
 } 
